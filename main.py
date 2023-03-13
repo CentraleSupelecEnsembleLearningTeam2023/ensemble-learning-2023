58369834699e6e1ee54393b6bb80e00969bda752
najemali <b00806315@essec.edu>
2023-03-13 19:57:43 +0100

    Applied preprocessing in main and linear reg

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 # % matplotlib inline
-
+from preprocessing import *
 from datetime import datetime
+from modeling import *
 
 
 import re
@@ -17,20 +18,42 @@ stopwords_ENG = stopwords.words('english')
 
 import pandas as pd
 
+keepcolumns = [ 'price','minimum_nights', 'number_of_reviews', 'neighbourhood_group',
+       'room_type','calculated_host_listings_count', 'reviews_per_month', 'neighbourhood',
+       'availability_365']
+
+num_features = ['price','minimum_nights','number_of_reviews','reviews_per_month',
+                'calculated_host_listings_count','availability_365']
+
 class APP():
-    def __init__(self, path, headnum=1, separator=','):
+    def __init__(self, path, keepcolumns ,numfeatures,headnum=1, separator=','):
         self.path = path
         self.headnum = headnum
         self.separator = separator
+        self.keepcolumns = keepcolumns
+        self.numfeatures = numfeatures
         self.df = self.load_data()
-
+        self.X_train,self.y_train,self.X_test,self.y_test = self.preprocess_data(self.df)
+        self.train_models()
     def load_data(self):
         # Read data from CSV file
         df = pd.read_csv(self.path, sep=self.separator, header=self.headnum)
         return df
+    
+        # @ANNABELLE please check this
+        # if __name__ == "__main__":
+        #     # Instantiate the DataReader class
+        #     reader = DataReader('/new-york-city-airbnb-open-data/AB_NYC_2019.csv')
+        #     # View first few columns of data
+        #     print(reader.df.head())
 
-if __name__ == "__main__":
-    # Instantiate the DataReader class
-    reader = DataReader('/new-york-city-airbnb-open-data/AB_NYC_2019.csv')
-    # View first few columns of data
-    print(reader.df.head())
+    def preprocess_data(self):
+        df = fill_missing_values(self.df)
+        df  = scale_data(df,num_features)
+        df = one_hot_data(df,['neighbourhood_group','room_type'])
+        df = encode_data(df,['neighbourhood'])
+        X_train,y_train,X_test,y_test = split_data(df,self.keepcolumns)
+        return X_train,y_train,X_test,y_test
+
+    def train_models(self):
+        train_linear_reg(self.X_train,self.y_train,self.X_test,self.y_test)
--- modeling.py
+++ modeling.py
@@ -1,1 +1,28 @@
-#Functions to be used for modeling and training
+# Functions to be used for modeling and training
+
+def reg_metrics(y_train,pred_train,y_test,pred_test):
+  '''
+  Function to calculate rmse and R^2 on training and out of sample data.
+  '''
+  from sklearn.metrics import mean_squared_error, r2_score
+
+  rmse_train = np.sqrt(mean_squared_error(y_train, pred_train))
+  r2_train = r2_score(y_train, pred_train)
+  print("Training RMSE:", rmse_train)
+  print("Training R²:", r2_train)
+
+  print("\n")
+
+  rmse_test = np.sqrt(mean_squared_error(y_test, pred_test))
+  r2_test = r2_score(y_test, pred_test)
+  print("Out-of-sample RMSE:", rmse_test)
+  print("Out-of-sample R²:", r2_test)
+
+def train_linear_reg(X_train,y_train,X_test,y_test):
+  from sklearn.model_selection import cross_val_score, KFold
+  from sklearn.linear_model import LinearRegression
+
+  lr = LinearRegression()
+
+  lr.fit(X_train,y_train)
+  reg_metrics(y_train,lr.predict(X_train),y_test,lr.predict(X_test))
