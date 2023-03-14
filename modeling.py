c020a36adc5e5bda1545046c1b85911e29c47cd8
karimelhage <karimelhage94@gmail.com>
2023-03-14 21:01:23 +0100

    "boxcox replaced with log tranform for preprocess"

--- modeling.py
+++ modeling.py
@@ -1,10 +1,16 @@
 # Functions to be used for modeling and training
+import numpy as np
 
-def reg_metrics(y_train,pred_train,y_test,pred_test):
+def reg_metrics(y_train,pred_train,y_test,pred_test,transform = False):
   '''
   Function to calculate rmse and R^2 on training and out of sample data.
+  Possibility to also transform data back to regular form since features
+  and targets log transformed for training
   '''
   from sklearn.metrics import mean_squared_error, r2_score
+
+  if transform == True:
+    y_train,pred_train,y_test,pred_test = np.e**y_train,np.e**pred_train,np.e**y_test,np.e**pred_test
 
   rmse_train = np.sqrt(mean_squared_error(y_train, pred_train))
   r2_train = r2_score(y_train, pred_train)
@@ -18,11 +24,7 @@ def reg_metrics(y_train,pred_train,y_test,pred_test):
   print("Out-of-sample RMSE:", rmse_test)
   print("Out-of-sample R²:", r2_test)
 
-def train_linear_reg(X_train,y_train,X_test,y_test, cross_val = False):
-  '''
-  Function to conduct linear regression model.
-  with option for cross validation
-  '''
+def train_linear_reg(X_train,y_train,X_test,y_test,cross_val = False, transform = False):
   from sklearn.model_selection import cross_val_score, KFold
   from sklearn.linear_model import LinearRegression
 
@@ -35,22 +37,15 @@ def train_linear_reg(X_train,y_train,X_test,y_test, cross_val = False):
 
   else:
     lr.fit(X_train,y_train)
-    reg_metrics(y_train,lr.predict(X_train),y_test,lr.predict(X_test))
+    reg_metrics(y_train,lr.predict(X_train),y_test,lr.predict(X_test),transform = transform)
 
 
-def train_decision_tree(X_train,y_train,X_test,y_test,max_depth= None,min_samples_split = 2,cross_val = False,grid_search = False):
-  '''
-  :param max_depth: set max_depth of decision tree
-  :param min_samples_split: set minimal samples per split
-  :param cross_val: boolean, provide option for cross validation
-  :param grid_search: boolean, provide option for grid search
-  :return: training decision tree
-  '''
+def train_decision_tree(X_train,y_train,X_test,y_test,max_depth= None,min_samples_split = 2,cross_val = False,grid_search = False,transform = False):
   from sklearn.model_selection import GridSearchCV, cross_val_score, KFold
   from sklearn.tree import DecisionTreeRegressor
 
   clf = DecisionTreeRegressor(max_depth = max_depth,min_samples_split = min_samples_split)
-
+  
   if cross_val == True:
     kfold = KFold(n_splits=10)
     print("Average kFold CV Score: {}".format(np.mean(cross_val_score(clf, X_train, y_train,
@@ -66,20 +61,15 @@ def train_decision_tree(X_train,y_train,X_test,y_test,max_depth= None,min_sample
 
   else:
     clf.fit(X_train,y_train)
-    reg_metrics(y_train,clf.predict(X_train),y_test,clf.predict(X_test))
+    reg_metrics(y_train,clf.predict(X_train),y_test,clf.predict(X_test),transform = transform)
 
-def train_random_forest(X_train,y_train,X_test,y_test,estimators = 100,max_depth= None,min_samples_split = 2,cross_val = False,grid_search = False):
-  '''
-  :param estimators: set number of estimators
-  :param max_depth: set max_depth
-  :param min_samples_split: set minimal samples of the split
-  :param cross_val: boolean, option for cross validation
-  :param grid_search: boolean, option for grid search
-  :return: training random forest
-  '''
+
+def train_random_forest(X_train,y_train,X_test,y_test,estimators = 100,max_depth= None,min_samples_split = 2,cross_val = False,grid_search = False,transform = False):
+  from sklearn.model_selection import GridSearchCV, cross_val_score, KFold
   from sklearn.ensemble import RandomForestRegressor
 
   rf = RandomForestRegressor(n_estimators = estimators,max_depth = max_depth,min_samples_split = min_samples_split)
+  
   if cross_val == True:
     kfold = KFold(n_splits=10)
     print(np.mean(cross_val_score(rf, X_train, y_train,
@@ -95,4 +85,4 @@ def train_random_forest(X_train,y_train,X_test,y_test,estimators = 100,max_depth
 
   else:
     rf.fit(X_train,y_train)
-    reg_metrics(y_train,rf.predict(X_train),y_test,rf.predict(X_test))
+    reg_metrics(y_train,rf.predict(X_train),y_test,rf.predict(X_test),transform = transform)
