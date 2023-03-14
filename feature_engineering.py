494591c831c12a583ca31835b5508a3930f241e3
karimelhage <karimelhage94@gmail.com>
2023-03-14 21:31:38 +0100

    "add count word list feature"

--- feature_engineering.py
+++ feature_engineering.py
@@ -14,3 +14,22 @@ def recency(df):
     recency = df_last_review['last_review'].max() - df_last_review['last_review']
 
     return recency.astype('timedelta64[D]')
+
+def count_words_in_listing(df):
+  ''' 
+  This function will take the string of name description of
+  each listing and count the number of words of that listing.
+  '''
+  df_transformed=df.copy()
+
+  return [len(str(name).split()) for name in df_transformed["name"]]
+
+
+def add_engineered_features(df):
+    '''
+  Function to add features engineered in aggregrate manner
+  '''
+    df_transformed = df.copy()
+    df_transformed['last_review_recency'] = recency(df_transformed)
+    df_transformed['count_words_in_listing'] = count_words_in_listing(df_transformed)
+    return df_transformed
