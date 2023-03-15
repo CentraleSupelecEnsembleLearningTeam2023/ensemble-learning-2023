5233b2d335e2e301266325af65d29eb59aecc8db
EmmaHongW <emma.hong.w@gmail.com>
2023-03-15 12:27:35 +0100

    Add the number of stations nearby feature

--- feature_engineering.py
+++ feature_engineering.py
@@ -1,5 +1,6 @@
 #Functions of engineered features
 import pandas as pd
+import numpy as np
 
 def recency(df):
     """
@@ -24,12 +25,66 @@ def count_words_in_listing(df):
 
   return [len(str(name).split()) for name in df_transformed["name"]]
 
+def distance(lat1, lon1, lat2, lon2):
+    '''
+    Calculates the distance in meters between two coordinates on Earth, given their latitudes and longitudes in degrees.
+
+    Args:
+    - lat1: float - latitude of the first point in degrees
+    - lon1: float - longitude of the first point in degrees
+    - lat2: float - latitude of the second point in degrees
+    - lon2: float - longitude of the second point in degrees
+    
+    Returns:
+    - distance: float - the distance between the two points in meters
+    '''
+    R = 6371
+    dlat = lat2 - lat1
+    dlon = lon2 - lon1
+    a = np.sin(dlat/2)*2 + np.cos(lat1) * np.cos(lat2) * np.sin(dlon/2)*2
+    c = 2 * np.arcsin(np.sqrt(a))
+    distance = R * c * 1000
+    
+    return distance
+
+def numstation(la, lo, df2):
+    '''
+    Counts the number of stations within a radius of 1 km, given a latitude and longitude representing a location, and a dataframe of public transport stations.
+
+    Args:
+    - la: float -  the latitude of the location
+    - lo: float -  the longitude of the location
+    - df2: pandas DataFrame - A dataframe of public transport stations with columns 'Station Latitude' and 'Station Longitude'
+
+    Returns:
+    - count: integer - the number of public transport stations within 1km of the location
+    '''
+    
+    lat2 = df2['Station Latitude'].values
+    lon2 = df2['Station Longitude'].values
+  
+    distances = distance(np.radians(la), np.radians(lo), np.radians(lat2), np.radians(lon2))
+
+    # Count the number of public transport stations within 1000 meters from the house
+    count = np.sum(distances <= 1000)
+    
+    return count
 
 def add_engineered_features(df):
     '''
   Function to add features engineered in aggregrate manner
   '''
+    # Read in the new york subway stations location data data
+    # Source: https://data.ny.gov/en/Transportation/NYC-Transit-Subway-Station-Map/6xm2-7ffy
+    stations = pd.read_csv('new-york-city-airbnb-open-data/NYC_Transit_Subway_Station_Location.csv')
+    #from tqdm import tqdm, tqdm_pandas
+    #tqdm.pandas()
+
     df_transformed = df.copy()
     df_transformed['last_review_recency'] = recency(df_transformed)
     df_transformed['count_words_in_listing'] = count_words_in_listing(df_transformed)
+    df_transformed['num_of_stations_nearby'] = df_transformed.apply(lambda row: numstation(row['latitude'], row['longitude'], stations), axis = 1)
+    # Use the following line if you want to track the progress
+    #df_transformed['num_of_stations_nearby'] = df_transformed.progress_apply(lambda row: numstation(row['latitude'], row['longitude'], stations), axis = 1)
+
     return df_transformed
